44e5aab165f5e9a517cd5658644e9c7372c92aab
Ratul Islam <iratul010@gmail.com>
2024-07-08 09:53:21 +0600

    heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,58 @@ import Sidebar from "../components/Sidebar";
 const Home = () => {
   return (
     <div className="content">
-       <Sidebar/>
-        <main className="hotel-view">
-          <h2>Hotel view</h2>
-        </main>
+      <Sidebar />
+      <main className="hotel-view">
+        <div className="gallery">
+          <figure className="gallery__item">
+            <img src="../src/assets/img/hotel-1.jpg" alt="Hotel photo 1" className="gallery__photo" />
+          </figure>
+          <figure className="gallery__item">
+            <img src="../src/assets/img/hotel-2.jpg" alt="Hotel photo 2" className="gallery__photo" />
+          </figure>
+          <figure className="gallery__item">
+            <img src="../src/assets/img/hotel-3.jpg" alt="Hotel photo 3" className="gallery__photo" />
+          </figure>
+        </div>
+        <div className="overview">
+          <h1 className="overview__heading">
+            Hotel Bd  Cox&apos;s  bazar
+          </h1>
+          <div className="overview__stars">
+            <svg className="overview__icon-star">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-star">
+              </use>
+            </svg>
+            <svg className="overview__icon-star">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-star">
+              </use>
+            </svg>
+            <svg className="overview__icon-star">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-star">
+              </use>
+            </svg>
+            <svg className="overview__icon-star">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-star">
+              </use>
+            </svg>
+            <svg className="overview__icon-star">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-star">
+              </use>
+            </svg>
+          </div>
+          <div className="overview__location">
+          <svg className="overview__icon-location">
+              <use xlinkHref="../src/assets/img/sprite.svg#icon-location-pin">
+              </use>
+            </svg>
+            <button className="btn-inline">Chattogram, Dhaka</button>
+          </div>
+          <div className="overview__rating">
+            <div className="overview__rating-average">8.6</div>
+            <div className="overview__rating-count"> 429 votes</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
